eadd036eb821b684e8c0884fd0a9385a65b458de
kevin.zhu <kevin.zhu@alo7.com>
2017-04-16 19:35:51 +0800

    微调 ballonmarker 的 y 方向位置

--- Source/Charts/Charts/ChartViewBase.swift
+++ Source/Charts/Charts/ChartViewBase.swift
@@ -636,7 +636,7 @@ open class ChartViewBase: NSUIView, ChartDataProvider, AnimatorDelegate
     /// - returns: The actual position in pixels of the MarkerView for the given Entry in the given DataSet.
     open func getMarkerPosition(highlight: Highlight) -> CGPoint
     {
-        return CGPoint(x: highlight.drawX, y: highlight.drawY)
+        return CGPoint(x: highlight.drawX, y: highlight.drawY - 4)
     }
     
     // MARK: - Animation
